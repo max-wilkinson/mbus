30c4f5b8eab2fb4a0dd7f45ec52afe11dba026af
Max Wilkinson <mawilkin@umich.edu>
2019-11-22 15:49:59 -0500

    Added client for stops

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import RouteClient from './routes/RouteClient';
 import EtaClient from './etas/EtaClient';
+import StopClient from './stops/StopClient';
+
+const etaClient = new EtaClient();
+etaClient.GetEtas(137).then(etas => {
+  // console.log(etas);
+});
 
 const routeClient = new RouteClient();
 routeClient.GetRoutes().then(routes => {
   // console.log(routes);
 });
 
-const etaClient = new EtaClient();
-etaClient.GetEtas(137).then(etas => {
-  // console.log(etas);
+const stopClient = new StopClient();
+stopClient.GetStops().then(stops => {
+  // console.log(stops);
 });
---
+++ src/stops/StopClient.ts
@@ -0,0 +1,29 @@
+import * as request from 'request-promise-native';
+import RouteClient from '../routes/RouteClient';
+import Route from '../routes/Route';
+import Stop from './Stop';
+
+export default class StopClient {
+  private readonly url = 'http://mbus.doublemap.com/map/v2/stops';
+  private readonly routeClient = new RouteClient();
+
+  public async GetStops() {
+    const routes = await this.routeClient.GetRoutes();
+
+    const response = await request.get(this.url);
+    const stops = JSON.parse(response);
+
+    return this.filterStops(stops, routes);
+  }
+
+  private filterStops(stops: Stop[], routes: Route[]): Stop[] {
+    let activeStops: Set<number> = new Set();
+    routes.forEach(route => {
+      route.stops.forEach(stop => {
+        activeStops.add(stop);
+      });
+    });
+
+    return stops.filter(stop => activeStops.has(stop.id));
+  }
+}
