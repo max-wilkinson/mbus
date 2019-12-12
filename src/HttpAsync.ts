a5c3670878ba30cacaa9365ddfdcfa25431731cd
Max Wilkinson <mawilkin@umich.edu>
2019-12-12 13:34:53 -0500

    Added types for HttpAsync module

--- src/HttpAsync.ts
+++ src/HttpAsync.ts
@@ -2,11 +2,11 @@ import http from 'http';
 
 export default function getAsync(url: URL): Promise<any> {
   return new Promise((resolve, reject) => {
-    http.get(url, response => {
+    http.get(url, (response: any) => {
       response.setEncoding('utf8');
 
       let output = '';
-      response.on('data', data => {
+      response.on('data', (data: string) => {
         output += data;
       });
 
@@ -15,7 +15,7 @@ export default function getAsync(url: URL): Promise<any> {
         resolve(responseObject);
       });
 
-      response.on('error', error => {
+      response.on('error', (error: any) => {
         reject(error);
       });
     });
